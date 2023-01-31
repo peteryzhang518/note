be34f25d16b4eec7fd38b8c675f7c467d30ac1fa
pz <peteryzhang518@gmail.com>
2023-01-31 23:58:28 +0000

    add mk file example

---
+++ c/test.mk
@@ -0,0 +1,17 @@
+hello:main.o test.o
+目标：依赖
+    gcc -o hello main.o test.o
+生成目标的命令
+
+
+
+main.o:main.c
+依赖：依赖的依赖
+    gcc -c main.c -o main.o
+生成依赖的命令
+
+
+test.o:test.c
+依赖：依赖的依赖
+    gcc -c test.c -o test.o
+生成依赖的命令
